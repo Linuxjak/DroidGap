eda4b20ff3b815bf8e9f4e8ea390716570546f6b
passionfactory <tragicidea@gmail.com>
2013-03-20 07:06:06 +0700

    first commit

---
+++ src/com/cobaphonegapandroid/MainActivity.java
@@ -0,0 +1,22 @@
+package com.cobaphonegapandroid;
+
+import android.os.Bundle;
+import android.view.Menu;
+import com.phonegap.*;
+
+public class MainActivity extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
